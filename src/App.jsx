e5ac49ec405a80a014c055f9f31804c0c66ecc02
Asif <asifjirayat@gmail.com>
2025-10-18 21:26:13 +0530

    Using side effects (Firebase)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
-
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 
 const App = () => {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch("https://redux-backend-f203b-default-rtdb.firebaseio.com/cart.json", {
+      method: "PUT",
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
 
   return (
     <Layout>
